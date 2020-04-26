7770f42060cac001d5ac0f793060249226258612
lenargum <lenargamgam@gmail.com>
2020-04-26 13:06:34 +0300

    now it is Chrome extension

--- public/background.js
+++ public/background.js
@@ -1,17 +1,17 @@
 const jsLocation = "./static/js/bundle.js";
 const cssLocation = null;
 
-browser.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
     if (changeInfo.status === "complete") {
         if (jsLocation !== null) {
-            browser.tabs.executeScript(tabId, {
+            chrome.tabs.executeScript(tabId, {
                 file: jsLocation,
                 runAt: "document_end"
             });
         }
 
         if (cssLocation !== null) {
-            browser.tabs.insertCSS(tabId, {
+            chrome.tabs.executeCSS(tabId, {
                 file: cssLocation,
                 runAt: "document_end"
             });
